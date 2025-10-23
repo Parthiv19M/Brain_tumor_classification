c6d558cd0ee64b68dd3d766d7e9448577b81950d
Parthiv19M <2320030380@klh.edu.in>
2025-10-24 00:58:09 +0530

    chore: add project structure and documentation

---
+++ brain_tumor_classification/tests/test_imports.py
@@ -0,0 +1,11 @@
+"""Test if all required packages can be imported."""
+def test_imports():
+    """Test that required packages can be imported."""
+    import numpy
+    import tensorflow
+    import pandas
+    import matplotlib
+    import sklearn
+    import cv2
+    import PIL
+    assert True
